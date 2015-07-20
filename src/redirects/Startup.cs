b8bbe58efa3f537b86882e482b209a5eeb1f4c82
Matthew Hancock <git@mt.hn>
2015-07-20 14:33:53 -0400

    Initial commit

---
+++ src/redirects/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.Runtime;
+using Newtonsoft.Json;
+
+namespace redirects {
+    public class Startup {
+        List<SiteConfig> sites;
+        public void Configure(IApplicationBuilder app, IApplicationEnvironment env) {
+            sites = JsonConvert.DeserializeObject<List<SiteConfig>>(Util.File.LoadToString(env.ApplicationBasePath, "sites.json"));
+            app.Run(Redirect);
+        }
+
+        private async Task Redirect(HttpContext context) {
+            var domainName = context.Request.Host.ToString();
+            if (domainName.StartsWith("www.")) {
+                domainName = domainName.Substring(4);
+            }
+            var config = sites.Find((c) => c.DomainName == domainName);
+            if (config != null) {
+                var url = config.IncludePath ? config.DomainName + context.Request.Path.ToString() : config.DomainName;
+                context.Response.Redirect(url, config.Permanent);
+            } else {
+                await context.Response.WriteAsync("Invalid Domain");
+            }
+        }
+    }
+    public class SiteConfig {
+        private string _domainname;
+        private bool _includePath, _permanent;
+
+        public SiteConfig(string DomainName, bool IncludePath, bool Permanent) {
+            _domainname = DomainName;
+            _includePath = IncludePath;
+            _permanent = Permanent;
+        }
+        public string DomainName { get { return _domainname; } }
+        public bool IncludePath { get { return _includePath; } }
+        public bool Permanent { get { return _permanent; } }
+    }
+}
---
+++ src/redirects/Util/File.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace redirects.Util {
+    public class File {
+        public static string LoadToString(string ApplicationBasePath, string FilePath) {
+            string text = null;
+            var filename = ApplicationBasePath + "/" + FilePath;
+            using (var sr = new StreamReader(filename)) {
+                text = sr.ReadToEnd();
+            }
+            return text;
+        }
+    }
+}
